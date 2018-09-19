a282bf3aa10a20747bdac7ff0a36c86c22ecbd57
Will Cronin <croninw9@students.rowan.edu>
2018-09-19 14:48:55 -0400

    Created Button Interrupt for MSP430G2553

---
+++ Button Interrupt/Lab_2ButtonInterrupt/main.c
@@ -0,0 +1,39 @@
+#include <msp430.h> 
+
+
+/**
+ * File: main.c
+ * Author: Will Cronin
+ * Date Created: September 19th 2018
+ * Date of Last Revision: September 19th 2018
+ * Board Used: MSP430G2553
+ */
+void main(void)
+{
+	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+	
+	P1SEL = 0;              //Set port 1 as a GPIO
+	P1DIR |= BIT0;          //Sets port 1.0 as an output
+	P1DIR &= ~(BIT3);       //Sets port 1.3 as an input
+
+	P1REN |= BIT3;          //Enable pull-up resistor
+	P1OUT &= ~BIT0;          //Sets output for pin 1.0
+
+	P1IE |= BIT3;           //Enable the interrupt on port 1.3
+	P1IES |= BIT3;          //Set as falling edge
+	P1IFG &= ~(BIT3);       //Clear interrupt flag
+
+	_BIS_SR(GIE);           //Enables Global Interrupt
+
+	while(1)
+	{
+
+	}
+}
+
+#pragma vector=PORT1_VECTOR
+__interrupt void PORT_1(void)
+{
+    P1OUT ^= 0x01;
+    P1IFG &= ~(BIT3);
+}
